9cd954077c56a743bf6b3e27b42ba275637dfd63
dgabruk <deathcore1315@gmail.com>
2024-09-12 20:15:53 +0300

    done

--- js/task-2.js
+++ js/task-2.js
@@ -4,10 +4,11 @@
 
 function makeArray(firstArray, secondArray, maxLength) {
     const newArray = firstArray.concat(secondArray);
-    if (newArray <= maxLength) {
-        return newArray;
-    } else {
+    if (newArray.length >= maxLength) {
         return newArray.slice(0, maxLength);
+    } else {
+        return newArray;
+        ;
     }
 }
 
